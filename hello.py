5ca169f85b4196d7a7ed10fbd37e3d55b12a53dd
DFoly <dan.foley@ucdconnect.ie>
2016-11-14 17:32:21 +0000

    Added webformn to home page

--- hello.py
+++ hello.py
@@ -3,25 +3,43 @@
 Created on Mon Nov 14 10:29:01 2016
 
 @author: dfoley
+// be careful wtform import Form is wrong
 """
-from flask import Flask, render_template
-from flask.ext.bootstrap import Bootstrap
-from flask.ext.moment import Moment
+from flask import Flask, render_template, redirect, session, url_for, flash
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import Form 
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/index')
+
+
+@app.route('/index', methods = ['GET', 'POST'])
 def index():
-    return render_template('index.html',
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Have you changed your name?')
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('index'))
+    return render_template('index.html', form = form, name = session.get('name'),
                            current_time = datetime.utcnow())
     
     
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name = name)
+    return render_template('user.html', name = name,
+                           current_time = datetime.utcnow())
     
 
 @app.errorhandler(404)
@@ -32,6 +50,10 @@ def page_not_Found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators = [DataRequired()])
+    submit = SubmitField('Submit')
     
 
     
