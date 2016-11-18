26650b0da5b3250fc70a6beeb6de81c395e32d9a
DFoly <dan.foley@ucdconnect.ie>
2016-11-18 16:50:12 +0000

    Added unit tests

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,4 @@
-# this is the app package constructor
-
-from flask import Flask, render_template
+from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from flask_moment import Moment
@@ -12,18 +10,19 @@ mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
 
-def create_app(config_name): # development, production, testing??
+
+def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
     bootstrap.init_app(app)
-    main.init_app(app)
+    mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
 
-    # NB put at end of function to avoid circular dependencies
-    from main import main as main_blueprint
+    from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     return app
+
--- config.py
+++ config.py
@@ -1,43 +1,45 @@
-# we store the configuration settings in this script
-
 import os
-basdir = os.path.abspath(os.path.dirname(__file__))
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-class config:
+class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-    BLOGG_MAIL_SUBJECT_PREFIX = '[Blogg]'
-    BLOGG_MAIL_SENDER = 'Blogg Admin <blogg@example.com>'
-    BLOGG_ADMIN = os.environ.get('BLOGG_ADMIN')
-
-    @staticmethod
-    def __init__(app):
-        pass
-
-class DevelopmentConfig(Config):
-    DEBUG = True
-    MAIL_SERVER = 'smtp.gogglemail.com'
+    MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-                              'sqlite:///' + os.path.join(basedir, 'date-dev.sqlite')
+        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
 
 class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
-                              'sqlite:///' + os.path.join(basedir, 'data_test.sqlite')
+        'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-                              'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
 
 config = {
-        'development': DevelopmentConfig,
-        'testing': TestingConfig,
-        'production': ProductionConfig,
-        'default': DevelopmentConfig
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
 
-    }
+    'default': DevelopmentConfig
+}
