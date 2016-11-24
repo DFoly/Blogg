d78448be06843d77de8e4599f772780f5fe6970b
DFoly <dan.foley@ucdconnect.ie>
2016-11-24 16:57:17 +0000

    Added registration form and link

--- app/__init__.py
+++ app/__init__.py
@@ -3,19 +3,20 @@ from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
-from config import config
 from flask_login import LoginManager
+from config import config
 
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
+
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
-login_manager.login_view =  'auth.login'
+login_manager.login_view = 'auth.login'
 
 
-def create_app(config_name): # is it test, production or development??
+def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
@@ -26,15 +27,10 @@ def create_app(config_name): # is it test, production or development??
     db.init_app(app)
     login_manager.init_app(app)
 
-    from .main import main as main_blueprint   # import global decorators
+    from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
-    
-    
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint, url_prefix = '/auth') 
-    # url_prefix all routes have prefix /auth eg /auth/login
-    # full url http://localhost:5000/auth/login
 
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
-
--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Nov 23 13:56:38 2016
-
-@author: dfoley
-"""
-
 from flask import Blueprint
 
 auth = Blueprint('auth', __name__)
