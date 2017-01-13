3a63ec86467280298de4aee99a9eb6896d4eaa92
DFoly <dan.foley@ucdconnect.ie>
2017-01-13 16:16:17 +0000

    Generated fake posts and users for testing

--- app/models.py
+++ app/models.py
@@ -64,6 +64,35 @@ class User(UserMixin, db.Model):
     avatar_hash = db.Column(db.String(32))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
+
+    #adds fake blog post data
+    @staticmethod
+    def generate_fake(count=100):
+        from sqlalchemy.exc import IntegrityError
+        from random import seed
+        import forgery_py
+
+        seed()
+        for i in range(count):
+            #generate random users + autoconfirm account
+            u = User(email = forgery_py.internet.email_address(),
+                    username = forgery_py.internet.user_name(True),
+                    password = forgery_py.lorem_ipsum.word(),
+                    confirmed = True,
+                    name = forgery_py.name.full_name(),
+                    location = forgery_py.address.city(),
+                    about_me = forgery_py.lorem_ipsum.sentence(),
+                    member_since = forgery_py.date.date(True))
+            db.session.add(u)
+            try:
+                db.session.commit()
+                # small chance that will be duplicates
+                #if so we rollback and do again
+            except IntegrityError:
+                db.session.rollback()
+
+
+
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
         if self.role is None:
@@ -162,6 +191,10 @@ class User(UserMixin, db.Model):
         return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
             url=url, hash=hash, size=size, default=default, rating=rating)
 
+
+
+
+
     def __repr__(self):
         return '<User %r>' % self.username
 
@@ -187,3 +220,20 @@ class Post(db.Model):
     body = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    @staticmethod
+    def generate_fake(count=100):
+        from random import seed, randint
+        import forgery_py
+
+        seed()
+        user_count = User.query.count()
+        for i in range(count):
+            #generate post from random user
+            u = User.query.offset(randint(0, user_count -1)).first()
+            p = Post(body = forgery_py.lorem_ipsum.sentences(randint(1,3)),
+                    timestamp=forgery_py.date.date(True),
+                    author = u)
+            db.session.add(p)
+            db.session.commit()
+
