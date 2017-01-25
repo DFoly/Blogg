f3bc3dbd9ef93328508dd9e8b72a7353deab718a
DFoly <dan.foley@ucdconnect.ie>
2017-01-25 11:29:34 +0000

    Blog post editor ease enter the commit message for your changes. Lines starting

--- app/main/views.py
+++ app/main/views.py
@@ -79,3 +79,28 @@ def edit_profile_admin(id):
     form.location.data = user.location
     form.about_me.data = user.about_me
     return render_template('edit_profile.html', form=form, user=user)
+
+
+# route for uniquew blog posts
+@main.route('/post/<int:id>')
+def post(id):
+    post = Post.query.get_or_404(id)
+    return render_template('post.html', posts = [post])
+
+
+# route to blog post editor
+@main.route('/edit/<int:id>', methods = ['GET', 'POST'])
+@login_required
+def edit(id):
+    post = Post.query.get_or_404(id)
+    if current_user != post.author and \
+        not current_user.can(Permission.ADMINISTER):
+            abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.body = form.body.data
+        db.session.add(post)
+        flash('The Post has been updated')
+        return redirect(url_for('post', id = post.id))
+    form.body.data = post.body
+    return redirect('edit_post.html', form = form)
--- app/models.py
+++ app/models.py
@@ -5,6 +5,8 @@ from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app, request
 from flask_login import UserMixin, AnonymousUserMixin
 from . import db, login_manager
+from markdown import markdown
+import bleach
 
 
 class Permission:
@@ -220,6 +222,7 @@ class Post(db.Model):
     body = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    body_html = db.Column(db.Text)
 
     @staticmethod
     def generate_fake(count=100):
@@ -236,4 +239,18 @@ class Post(db.Model):
                     author = u)
             db.session.add(p)
             db.session.commit()
+
+
+    @staticmethod
+    # initial conversion to markdown, clean removes any tags not in allowed
+    # tags. linkify converts urls in plain text to proper html links
+    def on_changed_body(target, value, oldvalue, initiator):
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
+                        'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
+                        'h1', 'h2', 'h3', 'p']
+        target.body_html = bleach.linkify(bleach.clean(markdown(value, output_format = 'html'),
+                            tags = allowed_tags, strip = True))
+
+
+db.event.listen(Post.body, 'set', Post.on_changed_body)
 
